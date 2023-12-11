1bb6764766e42e31b75201e09e47dc2bc21ec0c3
Melvyn OBAMBO <mobambo@louisarmand.local>
2023-12-11 14:55:30 +0100

    ajout du fichier de style principal

--- View/v_login.php
+++ View/v_login.php
@@ -4,7 +4,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/login.css">
         <link rel="icon" href="<?php echo ICON; ?>">
         <title>Se connecter</title>
     </head>
